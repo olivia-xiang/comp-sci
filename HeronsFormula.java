860e7aba33db790f041d3daf7146990f0594c850
Olivia Xiang <olivia.xiang@student.tdsb.on.ca>
2025-02-18 19:26:11 -0500

    fixed formatting

--- HeronsFormula.java
+++ HeronsFormula.java
@@ -1,19 +1,18 @@
 import java.util.Scanner;
-import java.lang.Math;
 
 public class HeronsFormula {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("Please enter the value of a: ");
-        double a = keyboard.nextDouble();
-        System.out.println("Please enter the value of b: ");
-        double b = keyboard.nextDouble();
-        System.out.println("Please enter the value of c: ");
-        double c = keyboard.nextDouble();
 
+        System.out.println("Please enter side a:");
+        double a = keyboard.nextDouble();
+        System.out.println("Please enter side b:");
+        double b = keyboard.nextDouble();
+        System.out.println("Please enter side c:");
+        double c = keyboard.nextDouble();
         keyboard.close();
 
         double area = 0.25 * Math.sqrt(4 * a * a * b * b - Math.pow((a * a + b * b - c * c), 2));
-        System.out.println("The area is " + area);
+        System.out.println("The area is: " + area);
     }
 }
