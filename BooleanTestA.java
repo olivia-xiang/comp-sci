17787a0fabd82302c649f39591cfc65d84a107d8
olivia-xiang <olivia.xiang@student.tdsb.on.ca>
2025-02-20 11:39:22 -0500

    Add files via upload

---
+++ BooleanTestA.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class BooleanTestA {
+    public static void main(String[] args) throws Exception {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Please enter input A:");
+        boolean a = keyboard.nextBoolean();
+        System.out.println("Please enter input B:");
+        boolean b = keyboard.nextBoolean();
+        System.out.println("Please enter input C:");
+        boolean c = keyboard.nextBoolean();
+        System.out.println("Please enter input D:");
+        boolean d = keyboard.nextBoolean();
+        keyboard.close();
+
+        boolean result = b && !d || !b && !c && d || !a && !b && !c || a && c && !d;
+        System.out.println("Your output is " + result);
+    }
+}
