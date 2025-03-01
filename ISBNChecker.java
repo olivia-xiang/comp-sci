6a0e3127e8fc424acf140b758096a67640c1a637
olivia-xiang <olivia.xiang@student.tdsb.on.ca>
2025-02-28 22:07:11 -0500

    Add files via upload

---
+++ ISBNChecker.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class ISBNChecker {
+    public static void main(String[] args) throws Exception {
+        Scanner keyboard = new Scanner(System.in);
+        double sum = 0;
+
+        System.out.println("Please enter an ISBN number:");
+        String n = keyboard.nextLine();
+
+        if (n.length() != 13) {
+            System.out.println("Your ISBN number is invalid");
+        } else {
+            for (int i = 0; i < n.length(); i++) {
+                if (i % 2 == 0) {
+                    sum += n.charAt(i) * 1;
+                } else {
+                    sum += n.charAt(i) * 3;
+                }
+            }
+            if (sum % 10 == 0) {
+                System.out.println("Your ISBN number is valid");
+            } else {
+                System.out.println("Your ISBN number is invalid");
+            }
+        }
+        keyboard.close();
+    }
+}
