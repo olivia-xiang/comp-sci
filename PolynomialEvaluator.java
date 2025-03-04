6597ce3ac9eb8773ef05b8d7e97fbfeca3c780bf
olivia-xiang <olivia.xiang@student.tdsb.on.ca>
2025-03-04 11:37:33 -0500

    Add files via upload

--- PolynomialEvaluator.java
+++ PolynomialEvaluator.java
@@ -10,7 +10,7 @@ public class PolynomialEvaluator {
         System.out.println("Please enter a polynomial in the form f(x)=");
 
         // don't hard code the equal sign positon make it more dynamic
-        StringTokenizer polyToken = new StringTokenizer ((keyboard.nextLine()).substring(5), " ");
+        StringTokenizer polyToken = new StringTokenizer((keyboard.nextLine()).substring(5), " ");
         System.out.println("Please enter an x value:");
         int x = keyboard.nextInt();
 
@@ -18,33 +18,48 @@ public class PolynomialEvaluator {
             coefValue = 0;
             boolean isMinus = false;
             String temp = polyToken.nextToken();
+
             if ((temp.charAt(0) == 43) || (temp.charAt(0) == 45)) {
-                // very redudanant make into if else 
-                if (temp.charAt(0) ==45) {
+                // very redudanant make into if else
+                if (temp.charAt(0) == 45) {
                     isMinus = true;
                 }
                 temp = temp.substring(1);
             }
+
             int coefLength = -1;
+            int coefLengthDeci = -1;
+
             for (int i = 0; i <= temp.length(); i++) {
                 if (Character.isDigit(temp.charAt(i))) {
-                    coefLength += 1;
+                    if (coefLengthDeci == -1) {
+                        coefLength += 1;
+                    } else {
+                        coefLengthDeci -= 1;
+                    }
+                } else if (temp.charAt(i) == '.') {
+                    coefLengthDeci = 0;
+                    continue;
                 } else {
                     break;
                 }
             }
-            for (int i = -1; i < coefLength; i++) {
-                coefValue += Character.getNumericValue(temp.charAt(i+1)) * Math.pow(10,coefLength);
-                coefLength --;
+            for (int i = -1; i < (coefLength + Math.abs(coefLengthDeci)); i++) {
+
+                double numValue = Character.getNumericValue(temp.charAt(i + 1));
+
+                if (coefLength >= 0) {
+                    coefValue += numValue * Math.pow(10, coefLength);
+                    coefLength--;
+                } else {
+                    coefValue += numValue * Math.pow(10, coefLengthDeci);
+                    coefLengthDeci++;
+                }
             }
-            // use the ternary operator??
-            if (isMinus == true) {
-                sum -= coefValue * Math.pow(x,Character.getNumericValue(temp.charAt(temp.length()-1)));
-            } else {
-                sum += coefValue * Math.pow(x,Character.getNumericValue(temp.charAt(temp.length()-1)));
-            }
+            double termValue = coefValue * Math.pow(x, Character.getNumericValue(temp.charAt(temp.length() - 1)));
+            sum = (isMinus) ? sum - termValue : sum + termValue;
         }
-        System.out.println(sum); 
+        System.out.println(sum);
         keyboard.close();
     }
 }
