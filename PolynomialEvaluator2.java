59747614411dcf599538cb3558d3803564b15e4e
olivia-xiang <olivia.xiang@student.tdsb.on.ca>
2025-03-05 22:09:23 -0500

    Update PolynomialEvaluator2.java

--- PolynomialEvaluator2.java
+++ PolynomialEvaluator2.java
@@ -8,7 +8,6 @@ public class PolynomialEvaluator2 {
 
         System.out.println("Please enter a poylnomial in the form f(x)=...");
         StringTokenizer poly = new StringTokenizer(keyboard.nextLine());
-
         System.out.println("Please enter an x value:");
         double x = keyboard.nextInt();
 
@@ -17,6 +16,8 @@ public class PolynomialEvaluator2 {
             int coeffStart = -1;
             int coeffEnd = -1;
             boolean isConst = true;
+            double expoValue = 0;
+            
             for (int i = 0; i < temp.length() ; i++) {
                 if (Character.isDigit(temp.charAt(i)) || temp.charAt(i) == '-') {
                     if (coeffStart == -1){
@@ -32,7 +33,7 @@ public class PolynomialEvaluator2 {
 
             double coeffValue = Double.parseDouble(temp.substring(coeffStart, coeffEnd));
 
-            double expoValue = 0;
+            //double expoValue = 0;
 
             expoValue = (isConst) ? 0 : Double.parseDouble(temp.substring(coeffEnd + 2, temp.length()));
 
