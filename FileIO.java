9f133427153d9e60efc5bfc7a9b498a692a51b5c
olivia-xiang <olivia.xiang@student.tdsb.on.ca>
2025-03-26 20:46:00 -0400

    Add files via upload

--- FileIO.java
+++ FileIO.java
@@ -7,8 +7,10 @@ import java.io.PrintWriter;
 public class FileIO {
     public static void main (String[]  args) {
         int sum = 0;
-        double counter = 0.0;
-        PrintWriter outputStream = null;
+        double counter = 6;
+        PrintWriter outputStream = null; 
+        BufferedReader inputStream = null;
+        String line = null;
 
         try {
             outputStream = new PrintWriter("text.txt");
@@ -18,35 +20,35 @@ public class FileIO {
             outputStream.println("4");
             outputStream.println("5");
             outputStream.println("6");
+            inputStream = new BufferedReader(new FileReader("text.txt"));
+            while ((line = inputStream.readLine()) != null) {
+                sum += Integer.parseInt(line);
+            } 
+        } catch (FileNotFoundException exception) {
+            System.out.println("Error opening file");
         } catch(IOException exception) {
             System.out.println("Problem creating or writing to the file");
         } finally {
             if (outputStream != null) {
                 outputStream.close();
             }
-        }
-        
-        BufferedReader inputStream = null;
-        String line = null;
-
-        try {
-            inputStream = new BufferedReader(new FileReader("text.txt"));
-            while ((line = inputStream.readLine()) != null) {
-                sum += Integer.parseInt(line);
-                counter++;
-            }
-        } catch (FileNotFoundException exception) {
-            System.out.println("Error opening file");
-        } catch (IOException exception) {
-            System.out.println("Error opening file");
-        } finally {
-            try { if (inputStream != null) {
-                inputStream.close();
+            try {
+                if (inputStream != null) {
+                    inputStream.close();
                 }
             } catch (IOException exception) {
                 System.out.println("Error opening file");
             }
         }
-        System.out.println(sum / counter);
+        try{    
+            outputStream = new PrintWriter("sum.txt");
+            outputStream.println(sum / counter);
+        } catch (FileNotFoundException exception) {
+            System.out.println("Error opening file");
+        } finally {
+            if (outputStream != null) {
+                outputStream.close();
+            }
+        }
     }
 }
