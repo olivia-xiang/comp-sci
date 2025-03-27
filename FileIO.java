99cbfc7101f8c858e81b1a777e30b9169798e1b0
olivia-xiang <olivia.xiang@student.tdsb.on.ca>
2025-03-27 10:27:46 -0400

    Add files via upload

--- FileIO.java
+++ FileIO.java
@@ -7,22 +7,18 @@ import java.io.PrintWriter;
 public class FileIO {
     public static void main (String[]  args) {
         int sum = 0;
-        double counter = 6;
+        double counter = 0;
         PrintWriter outputStream = null; 
         BufferedReader inputStream = null;
         String line = null;
 
         try {
-            outputStream = new PrintWriter("text.txt");
-            outputStream.println("2");
-            outputStream.println("1");
-            outputStream.println("3");
-            outputStream.println("4");
-            outputStream.println("5");
-            outputStream.println("6");
             inputStream = new BufferedReader(new FileReader("text.txt"));
             while ((line = inputStream.readLine()) != null) {
                 sum += Integer.parseInt(line);
+                counter++;
+            outputStream = new PrintWriter("sum.txt");
+            outputStream.println(sum / counter);
             } 
         } catch (FileNotFoundException exception) {
             System.out.println("Error opening file");
@@ -38,16 +34,6 @@ public class FileIO {
                 }
             } catch (IOException exception) {
                 System.out.println("Error opening file");
-            }
-        }
-        try{    
-            outputStream = new PrintWriter("sum.txt");
-            outputStream.println(sum / counter);
-        } catch (FileNotFoundException exception) {
-            System.out.println("Error opening file");
-        } finally {
-            if (outputStream != null) {
-                outputStream.close();
             }
         }
     }
