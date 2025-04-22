b82d9fdbd5fddc95bc512c4ebb508367c1d8ace6
olivia-xiang <olivia.xiang@student.tdsb.on.ca>
2025-04-22 11:40:07 -0400

    Add files via upload

---
+++ Drink.java
@@ -0,0 +1,75 @@
+public class Drink {
+
+    protected String name;
+    protected double[] volumes;
+    protected int calories;
+    private double price = 0.25;
+
+    public Drink(String name, double volume, int calories) {
+        this.name = name;
+        this.volumes[0] = volume;
+        this.volumes[1] = volume;
+        this.calories = calories;
+    }
+
+    protected String getName() {
+        return name;
+    }
+
+    protected double getVolumeTotal() {
+        return volumes[1];
+    }
+
+    protected double getVolumeLeft() {
+        return volumes[0];
+    }
+
+    protected int getCalories() {
+        return calories;
+    }
+
+    public void drink(double volumeDrank) {
+        if (volumes[0] == 0) {
+            System.out.println("Your drink is empty.");
+        } else if (volumeDrank <= volumes[0]) {
+            System.out.println(
+                    "You drank " + volumeDrank + "mL, that was " + volumeDrank / volumes[0] * calories + calories
+                            + " calories.");
+            volumes[0] -= volumeDrank;
+
+        } else {
+            volumes[0] = 0;
+            System.out.println("You can't drink more than the cup can hold. Your drink is now empty. You consumed "
+                    + calories + " calories.");
+        }
+    }
+
+    public void cashReciever(double money) {
+        if (money >= (volumes[1] - volumes[0]) * price) {
+            money -= (volumes[1] - volumes[0]) * price;
+            refillDrink(true, money);
+        }
+        refillDrink(false, money);
+    }
+
+    public void creditReciever(String cardNumer) {
+        for (int i = 0; i < cardNumer.length(); i++) {
+            int temp = 0;
+            if (i % 2 == 1) {
+                cardNumer.charAt(i) * 2;
+            }
+        }
+    }
+
+    private double refillDrink(boolean enoughPaid, double change) {
+        if (enoughPaid) {
+            System.out.println("Your drink has been refilled for the price of $" + (volumes[1] - volumes[0]) * price);
+            volumes[0] = volumes[1];
+            return change;
+        } else {
+            System.out.println("It costs " + volumes[1] * price + " to refill your drink. You need to enter $"
+                    + ((volumes[1] - volumes[0]) * price - change) + " more.");
+            return change;
+        }
+    }
+}
