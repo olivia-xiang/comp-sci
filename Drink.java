42bb00a1a18156549c66b4ca7b78475056437821
olivia-xiang <olivia.xiang@student.tdsb.on.ca>
2025-04-23 12:00:55 -0400

    Add files via upload

--- Drink.java
+++ Drink.java
@@ -1,9 +1,9 @@
 public class Drink {
 
     protected String name;
-    protected double[] volumes;
+    protected double[] volumes = new double[2];
     protected int calories;
-    private double price = 0.25;
+    private double price = 0.01;
 
     public Drink(String name, double volume, int calories) {
         this.name = name;
@@ -35,6 +35,7 @@ public class Drink {
             System.out.println(
                     "You drank " + volumeDrank + "mL, that was " + volumeDrank / volumes[0] * calories + calories
                             + " calories.");
+            calories -= volumeDrank / volumes[0] * calories;
             volumes[0] -= volumeDrank;
 
         } else {
@@ -53,12 +54,20 @@ public class Drink {
     }
 
     public void creditReciever(String cardNumer) {
+        int sum = 0;
         for (int i = 0; i < cardNumer.length(); i++) {
-            int temp = 0;
-            if (i % 2 == 1) {
-                cardNumer.charAt(i) * 2;
+            if (i % 2 == 0) {
+                if (cardNumer.charAt(i) * 2 > 9) {
+                    String[] nums = String.valueOf(cardNumer.charAt(i) * 2).split("");
+                    for (String num : nums) {
+                        sum += Integer.parseInt(num);
+                    }
+                } else {
+                    sum += cardNumer.charAt(i) * 2;
+                }
             }
-        }
+        } 
+        refillDrink((sum % 10 == 0 ? true : false), 0);
     }
 
     private double refillDrink(boolean enoughPaid, double change) {
@@ -67,7 +76,7 @@ public class Drink {
             volumes[0] = volumes[1];
             return change;
         } else {
-            System.out.println("It costs " + volumes[1] * price + " to refill your drink. You need to enter $"
+            System.out.println("It costs " + (volumes[1] - volumes[0]) * price + " to refill your drink. You need to enter $"
                     + ((volumes[1] - volumes[0]) * price - change) + " more.");
             return change;
         }
